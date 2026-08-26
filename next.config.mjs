0f6e276d2485c6f04c5d478f6d2a85c0be5d540b
prakashjung-devloper <jungprakash426@gmail.com>
2026-08-26 21:17:20 +0545

    build: static asset pipeline config for github pages workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  
+  basePath: '/Travel-website',
+  assetPrefix: '/Travel-website',
+  
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
